307fdadd9d497cfc78778522e7f853dcd307263d
nilsvandervelden <nilsvandervelden98@gmail.com>
2022-09-10 17:31:18 +0200

    added startup to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { setupFastifyAdapter } from './config/fastify/setup-fastify-adapter';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
+import { configService } from './config/config.service';
 import { AppModule } from './app.module';
 
+
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: false, transform: true }),
+    );
+
+    await app.listen(configService.getPort(), '0.0.0.0');
+
+  } catch (exception) {
+    console.log(exception);
+    if (exception.code == 'ECONNREFUSED') {
+      console.log('Failed to make a database connection. ' + 'Check the database connection settings and make sure the database server is active')
+    } else {
+      console.log('Exception occurred in the application');
+    }
+  }
 }
+
+
 bootstrap();

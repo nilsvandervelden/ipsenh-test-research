76921913eae9235bec0e3162afdae26a61e55ef0
nilsvandervelden <nilsvandervelden98@gmail.com>
2022-09-16 16:23:43 +0200

    wip

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,38 +6,47 @@ import { UserEntity } from '../src/modules/users/user.entity';
 import { TestingModule, Test } from '@nestjs/testing';
 import { UserModule } from '../src/modules/users/user.module';
 import { AppModule } from '../src/app.module';
+import { configService } from '../src/config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from '../src/modules/users/user.service';
+import { UserController } from '../src/modules/users/user.controller';
 
 describe('User module test', () => {
   let app: INestApplication;
   let userRepository: Repository<UserEntity>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, UserModule],
+      imports: [
+        TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+        TypeOrmModule.forFeature([
+          UserEntity,
+        ]),
+      ],
+      controllers: [UserController],
+      providers: [UserService],
+      // exports: [UserService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
 
-    beforeAll(async () => {
-      userRepository = await app.get<Repository<UserEntity>>(
-        getRepositoryToken(UserEntity),
-      );
-    });
+    userRepository = await app.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
+
+    afterAll(async () => {
+      await Promise.all([
+        app.close(),
+      ])
+    })
   });
 
   // TODO include test to ensure set requires functions
-  it('/ (POST) Should create a user', async () => {
+  it('Should get a empty array of users', async () => {
     await request(app.getHttpServer())
-      .post('/users')
-      .send({
-        email: 'test@test.nl',
-        password: 'password',
-      })
-      .expect({
-        email: 'test@test.nl',
-      })
-      .expect(201);
+      .get('/users')
+      .expect(404);
   });
 
   // afterAll(async () => {
